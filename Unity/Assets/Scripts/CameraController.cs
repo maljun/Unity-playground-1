b4ac87bd7be999e38fe798c6af7cca23b32b5a2f
Markus Ljung <markus.ljung@gmail.com>
2015-01-23 21:10:33 +0100

    add unity project

---
+++ Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float v = Input.GetAxis("Vertical");
+	    float h = Input.GetAxis("Horizontal");
+
+        Debug.Log("V: " + v + "H: " + h);
+
+        
+	}
+}
